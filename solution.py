a621940d31097347a0c5eab2d1aaf84a44881379
Kooreis <kooreis.official@gmail.com>
2024-12-14 09:13:00 -0600

    The second part of the script adds a method to the Graph class that allows edges to be added between vertices.

--- solution.py
+++ solution.py
@@ -1,6 +1,2 @@
-from collections import defaultdict
-
-class Graph:
-    def __init__(self, vertices):
-        self.graph = defaultdict(list)
-        self.V = vertices
+def addEdge(self, u, v):
+        self.graph[u].append(v)
