87aff3dd207cefb5866e6a30d4af48ef47111986
Kooreis <kooreis.official@gmail.com>
2024-12-14 09:13:00 -0600

    Definition of the depth-first search (DFS) method. This method is used to find a cycle in the graph. It marks each visited vertex with a color (1 for visited but not fully explored, 2 for fully explored), and keeps track of the parent of each vertex. If it encounters a vertex that has been visited but not fully explored, it has found a cycle.

--- solution.java
+++ solution.java
@@ -1,7 +1,16 @@
-import java.util.*;
-
-public class Main {
-    static int V;
-    static int[] color;
-    static int[] par;
-    static int cycle_start, cycle_end;
+static boolean dfs(int v, ArrayList<Integer>[] adj) {
+        color[v] = 1;
+        for (Integer u : adj[v]) {
+            if (color[u] == 0) {
+                par[u] = v;
+                if (dfs(u, adj))
+                    return true;
+            } else if (color[u] == 1) {
+                cycle_end = v;
+                cycle_start = u;
+                return true;
+            }
+        }
+        color[v] = 2;
+        return false;
+    }
